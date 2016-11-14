8cb808bf4da2fa9f9110fcba5b4c9d1b0b19a516
Norman Coloma <ua.norman@gmail.com>
2016-11-14 23:34:57 +0100

    First commit

---
+++ src/main/java/books/Book.java
@@ -0,0 +1,17 @@
+package main.java.books;
+
+/**
+ * Created by norman on 14/11/16.
+ */
+public class Book {
+
+    private String name;
+
+    public Book(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
